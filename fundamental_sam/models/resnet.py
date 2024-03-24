b823f74be0466d5aae6a10422ef898dc2efa7efb
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-03-24 21:40:47 +0700

    update

--- fundamental_sam/models/resnet.py
+++ fundamental_sam/models/resnet.py
@@ -76,6 +76,8 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
+        self.in_planes = 64
+        
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
