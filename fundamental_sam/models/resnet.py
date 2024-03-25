3011e3c07e4df9a9824463dd15229b8b9fc9760e
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-03-25 09:38:43 +0700

    down sample resnet

--- fundamental_sam/models/resnet.py
+++ fundamental_sam/models/resnet.py
@@ -81,6 +81,8 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -100,6 +102,7 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         out = self.activate(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
